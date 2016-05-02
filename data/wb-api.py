7da7a9c0262ebadd555d39b9ca2f9c5f82e8c5fa
Ricki@mac <rickixie@users.noreply.github.com>
2016-05-02 03:03:14 -0700

    update data

--- data/wb-api.py
+++ data/wb-api.py
@@ -15,46 +15,60 @@ import csv
 country_string = "LIC;MIC;LMC;UMC;HIC"
 
 #define indicators
-#education
+#education1
 # indicator_string = "SE.SCH.LIFE.FE;SE.SCH.LIFE.MA;SE.ADT.1524.LT.FE.ZS;SE.ADT.1524.LT.MA.ZS;SE.PRM.CMPT.FE.ZS;SE.PRM.CMPT.MA.ZS"
+#educaion2
 #economic
 #indicator_string = "SL.TLF.ACTI.1524.FE.ZS;SL.TLF.ACTI.1524.MA.ZS;SL.EMP.WORK.FE.ZS;SL.EMP.WORK.MA.ZS;SL.FAM.WORK.FE.ZS;SL.FAM.WORK.MA.ZS"
 #agency
-indicator_string =  "SP.M18.2024.FE.ZS;SP.ADO.TFRT;SP.DYN.CONU.ZS"
+#indicator_string =  "SP.M18.2024.FE.ZS;SP.ADO.TFRT;SP.DYN.CONU.ZS"
+indicator_string = "SP.ADO.TFRT"
 
 #list of indicators {id:name}
 # Category1: Education 
-#
+#	education1
 # 	{SE.SCH.LIFE.FE:"Expected year of schooling, female"}
 # 	{SE.SCH.LIFE.MA:"Expected year of schooling, male"}
-#	
-#	{SE.ADT.1524.LT.FE.ZS:"Youth Literacy Rate (%of population age 15-24), female"}
-#	{SE.ADT.1524.LT.MA.ZS:"Youth Literacy Rate (%of population age 15-24), male"}
-#	
+
+#	education2
 # 	{SE.PRM.CMPT.FE.ZS: "Primary completion rate, female(%)"}
 # 	{SE.PRM.CMPT.MA.ZS: "Primary completion rate, male(%)"}
 
+#	education3
+#	{SE.PRM.UNER.FE:"Children out of school, primary, female"}
+#	{SE.PRM.UNER.MA:"Children out of school, primary, male"}
+
+#	
+#
+
 
 # Category2: Economic Opportunity
-#
+#	economic1
 #	{SL.TLF.ACTI.1524.FE.ZS:"Labor force participation rate, % of populationa ages 15+, female"}
 #	{SL.TLF.ACTI.1524.MA.ZS:"Labor force participation rate, % of populationa ages 15+, male"}
-#
-#	{SL.EMP.WORK.FE.ZS:"Wage and salaried workers, female (% of females employed)"}
-#	{SL.EMP.WORK.MA.ZS:"Wage and salaried workers, female (% of males employed)"}
-#
-# 	{SL.FAM.WORK.FE.ZS: "Contributing family workers (% of employed ages 15+), female"}
-#	{SL.FAM.WORK.MA.ZS: "Contributing family workers (% of employed ages 15+), female"}
+
+#	economic2
+#	{SL.UEM.TOTL.FE.ZS:"Unemployment, female (% of females labor force)"}
+#	{SL.UEM.TOTL.MA.ZS:"Unemployement, male (% of males force)"}
+
 #
 
-# Category3: Agency 
-#
+# Category3: Agency
+#	no
 # 	{SP.M18.2024.FE.ZS:"Women first married by age 18"}
-#	
+#	agency1
 #	{SP.ADO.TFRT:"Adolescent fertility rate (birth per 1000, age 15-19)"}
-#	
+#	no
 # 	{SP.DYN.CONU.ZS: "Contraceptive prevalence (% of women ages 15-49)"}
 #
+
+# Category4: Public Life & Decision Making
+#	no
+# 	{SG.GEN.PARL.ZS:"Seats held by women in national parliament"}
+#	
+
+#
+
 
 
 #define source: Gender Statistics is 14
@@ -66,7 +80,7 @@ endYr = "2013"
 
 #define request url
 #http://api.worldbank.org/countries/lic;mic;lmc;umc;lmy;hic/indicators/SP.ADO.TFRT;SH.STA.BRTC.ZS?source=14&date=1965:2014&per_page=50&format=json
-url = "http://api.worldbank.org/countries/LIC;MIC;LMC;UMC;HIC/indicators/SP.M18.2024.FE.ZS;SP.ADO.TFRT;SP.DYN.CONU.ZS?source=14&date=2004:2013&per_page=600&format=json"
+url = "http://api.worldbank.org/countries/LIC;MIC;LMC;UMC;HIC/indicators/SP.ADO.TFRT?source=14&date=2004:2013&per_page=600&format=json"
 # url = "http://api.worldbank.org/countries/%(country_stringP)s/indicators/%(indicator_stringP)s?source=%(sourceP)s&date=%(startYrP)s:%(endYrP)s&per_page=50&format=json"%{"country_stringP":country_string, "indicator_stringP" = indicator_string, "sourceP" = source, "startYrP"=startYr, "endYrP"=endYr}
 
 response = requests.get(url)
